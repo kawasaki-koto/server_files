9b9004e60432d83006f9f510b60f21f245a8a64f
kawasaki-koto <koto.gyoniku@gmail.com>
2024-10-23 11:55:10 +0900

    edit

--- diffusers_setup.py
+++ diffusers_setup.py
@@ -1,3 +1,6 @@
-import i2i
+from diffusers import AutoPipelineForImage2Image, EulerAncestralDiscreteScheduler
+import torch
 
-i2i.create_pipe("stabilityai/sdxl-turbo")
+pipe = AutoPipelineForImage2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+# pipe.to("cuda")
